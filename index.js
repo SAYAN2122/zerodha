03d7643498a8b5373890dd8efd51daf0e6dca7e3
Sayan Goel <sayangoel14@gmail.com>
2026-01-19 18:05:37 +0530

    Added Authentication In Backend

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+/* ===== MongoDB Connection ===== */
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+/* ===== Routes ===== */
+app.get("/", (req, res) => {
+  res.send("Zerodha Backend Running");
+});
+
+app.use("/api/auth", authRoutes);
+
+/* ===== Render Port ===== */
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
